d2dced1878b33fa539dd2816b77a8a978fb3b143
AkshatVH <akshatvh@gmail.com>
2023-07-25 13:35:35 +0530

    Firt Commit & Creation of App.py

---
+++ app.py
@@ -0,0 +1,58 @@
+# from flask import Flask
+# ### WSGI Application 
+# app = Flask(__name__)
+
+# @app.route('/')
+# def welcome():
+#     return "Hello from Flask, Just don't break me!! AARRAA AARRA"
+
+# @app.route('/members')
+# def Members():
+#     return "Welcome to the AARRAA ARRAA page"
+
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+### Episode 2
+
+"""
+    Building Url Dynamically 
+    Variable Rules and URL Building
+"""
+
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def Welcome():
+    return "Flask Welcomes you!!"
+
+'''
+We are building the URL Dynamically using variable rules.
+
+'''
+@app.route('/Success/<int:score>')     
+def Success(score):
+    return "The person has passed and the marks is " + str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "the person has failed and the marks is " + str(score)
+    
+
+@app.route('/Results/<int:marks>')
+def Results(marks):
+    result = ""
+    if marks<30:
+        result =  'fail'
+    else :
+        result = 'Success'
+
+    return redirect(url_for(result, score= marks))
+
+if __name__ == "__main__":
+    app.run(debug= True)
